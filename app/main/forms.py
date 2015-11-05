112233c44f5fbd8ac779e031fbcd0b10a11896e4
Matthew Cox <matthew.cox@teachforamerica.org>
2015-11-05 09:37:45 -0600

    Getting form for schedules to work

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask.ext.wtf import Form
+from wtforms import Form as NoCsrfForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField, IntegerField, HiddenField
+    SubmitField, IntegerField, HiddenField, FieldList, FormField
 from wtforms_components import TimeField
 from wtforms.validators import Required
 from app.models import Grade, Subject, Period, Teacher, Section
@@ -35,11 +36,13 @@ class ScheduleDayForm(Form):
     name = StringField('Schedule Day', validators=[Required()])
     submit = SubmitField('Add/Modify')
 
+
 class PeriodForm(Form):
     number = IntegerField('Number', validators=[Required()])
     start_time = TimeField('Start Time', validators=[Required()])
     end_time = TimeField('End Time', validators=[Required()])
     submit = SubmitField('Add/Modify')
+
 
 class SectionForm(Form):
     name = StringField('Section Name', validators=[Required()])
@@ -57,12 +60,23 @@ class SectionForm(Form):
         self.teacher.choices = [(x.id, x.name) for x in Teacher.query.all()]
         self.period.choices = [(x.id, x.number) for x in Period.query.all()]
 
-class ScheduleForm(Form):
+
+class ScheduleFormItem(NoCsrfForm):
     student = HiddenField()
     schedule_day = HiddenField()
-    section = SelectField("Section", validators=[Required()])
+    section = SelectField("Section", coerce=int)
+    
+    def __init__(self, *args, **kwargs):
+        super(ScheduleFormItem, self).__init__(*args, **kwargs)
+        self.section.choices = [ (s.id, s.name) for s in Section.query.all()]
+        # self.section.choices = [ Section.query.all()]
+        print(self.section.choices)
 
-    def __init__(self, student, *args, **kwargs):
-        super(ScheduleForm, self).__init__(*args, **kwargs)
-        self.section.choices = [(x.id, x.name) for x in Section.query.all()]
-        student.data = student.id
+
+class SchedulePeriodGroup(NoCsrfForm):
+    schedule_days = FieldList(FormField(ScheduleFormItem))
+    period_num = None
+
+
+class ScheduleForm(Form):
+    periods = FieldList(FormField(SchedulePeriodGroup))
--- app/main/schedules.py
+++ app/main/schedules.py
@@ -1,37 +1,52 @@
-from ..models import Schedule, Student, Period
+from ..models import Schedule, Student, Period, Section, ScheduleDay
 from . import main
 from flask import render_template, url_for, flash, redirect
 from .. import db
-from .forms import ScheduleForm
+from .forms import ScheduleForm, ScheduleFormItem, SchedulePeriodGroup
 import pdb
 
 
-@main.route('/schedule/<int:student_id>', methods=['GET', 'POST'])
+@main.route('/schedule/<int:student_id>', methods=['GET'])
 def edit_schedule(student_id):
-    form = ScheduleForm(student=Student.query.get_or_404(student_id))
-    # pdb.set_trace()
+    form = ScheduleForm()
+    student = Student.query.get_or_404(student_id)
+    period_list = list()
+    schedule_day_list = ScheduleDay.query.all()
+
+    period_group = SchedulePeriodGroup()
+
+    for schedule_day in schedule_day_list:
+        item = ScheduleFormItem()
+
+        period_group.schedule_days.append_entry(item)
+
+    for period in Period.query.all():
+
+        cur_period_list = [(x.id, x.name)
+                           for x in Section.query.filter_by(period=period).all()]
+        period_list.append((period.number, cur_period_list))
+
+        form.periods.append_entry(period_group)
+
+    for i, period_select in enumerate(period_list):
+        number, choices = period_select
+        for j, schedule_day in enumerate(schedule_day_list):
+            form.periods[i].period_num = number
+            form.periods[i].schedule_days[j].section.choices = choices
+            form.periods[i].schedule_days[j].student.data = student.id
+            form.periods[i].schedule_days[j].schedule_day.data = schedule_day.id
+    return render_template('schedule_form.html', form=form, schedule_day_list=schedule_day_list)
+
+
+
+@main.route('/schedule/<int:student_id>', methods=['POST'])
+def edit_schedule_post(student_id):
+    form = ScheduleForm()
+    student = Student.query.get_or_404(student_id)
+
     if form.validate_on_submit():
-        schedule = Schedule()
-        schedule.name = form.name.data
-        db.session.add(schedule)
-        db.session.commit()
-        return redirect(url_for('.schedules'))
-    return render_template('schedule_form.html', form=form, periods=Period.query.all())
-
-
-# @main.route('/schedules/')
-# def schedules():
-#     schedules = Schedule.query.all()
-#     return render_template('all_schedules.html', schedules=schedules)
-
-
-# @main.route('/schedules/<int:id>', methods=['GET', 'POST'])
-# def edit_schedule(id):
-#     schedule = Schedule.query.get_or_404(id)
-#     form = ScheduleForm(name=schedule.name)
-#     if form.validate_on_submit():
-#         schedule.name = form.name.data
-#         db.session.add(schedule)
-#         flash('The schedule has been updated.')
-#         return redirect(url_for('.schedules'))
-#     return render_template('schedule_form.html', form=form, schedule=schedule)
+        print("Choice for period 1, schedule day 1 is " + str(form.periods[0].schedule_days[0].section.data))
+        print("Student for period 1, schedule day 1 is " + str(form.periods[0].schedule_days[0].student.data))
+        print("Schedule Day for period 1, schedule day 1 is " + str(form.periods[0].schedule_days[0].schedule_day.data))
+        return redirect(url_for('.students'))
+    return render_template('schedule_form.html', form=form, schedule_day_list=schedule_day_list)
