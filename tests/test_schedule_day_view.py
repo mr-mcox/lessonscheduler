ac990c93922aa772b0f62b0a13a4da0bf1671c1d
Matthew Cox <matthew.cox@teachforamerica.org>
2015-11-18 20:04:29 -0600

    Actually running selenium for reals

--- tests/test_schedule_day_view.py
+++ tests/test_schedule_day_view.py
@@ -1,11 +1,24 @@
 from flask import url_for
 import pytest
 import urllib
+from selenium import webdriver
 
 
 @pytest.mark.usefixtures('live_server')
 class TestLiveServer:
 
-    def test_server_is_up_and_running(self, app):
-        res = urllib.request.urlopen(url_for('main.index', _external=True))
-        assert res.code == 200
+    browser = None
+
+    @pytest.fixture(scope='class')
+    def browser(self, request):
+        browser = webdriver.Firefox()
+
+        def quit_browser():
+            browser.quit()
+
+        request.addfinalizer(quit_browser)
+        return browser
+
+    def test_server_is_up_and_running(self, app, browser):
+        browser.get(url_for('main.index', _external=True))
+        assert 'Lesson Scheduler' in browser.title
